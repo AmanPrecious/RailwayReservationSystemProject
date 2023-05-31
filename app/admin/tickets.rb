afa8475dafed3cae727ea5d2ab9099d334fb31a6
AmanPrecious <aman.ahirwar@preciousinfosystem.com>
2023-05-31 19:22:04 +0530

    add passenger detail and payment model generate

---
+++ app/admin/tickets.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Ticket do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :train_id, :booking_status, :booking_date, :from_station, :to_station, :class_type, :seat_type, :seat_no, :mobile, :email
+  #
+  # or
+  #
+  permit_params do
+   permitted = [:user_id, :train_id, :booking_status, :booking_date, :from_station, :to_station, :class_type, :seat_type, :seat_no, :mobile, :email]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+   end
+   
+  
+end
