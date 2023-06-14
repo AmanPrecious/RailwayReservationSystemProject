c42e70a8f2f7071917436f1083ada3324a73dd2c
AmanPrecious <aman.ahirwar@preciousinfosystem.com>
2023-06-14 19:08:04 +0530

    complete passenger add details, remove and seat allocation and updation

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,5 +1,7 @@
 class PassengersController < ApplicationController
+
   before_action :authenticate_user!
+
   def index
     @passengers = Passenger.find_by(ticket_id:params[:id])
   end
@@ -31,6 +33,7 @@ class PassengersController < ApplicationController
   end
 
   def destroy
+    
     @passenger = Passenger.find(params[:id])
     @ticket = Ticket.find(@passenger.ticket_id)
     @passenger.destroy
@@ -40,18 +43,18 @@ class PassengersController < ApplicationController
   def create
     @ticket = Ticket.find(params[:ticket_id])
     @passenger = Passenger.new(passenger_params.merge!(ticket:@ticket))
-    set_seat_no(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
+    #set_seat_no(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
    
     if params[:continue].present?
       if @passenger.save
-       update_seat_availibility(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
+       #update_seat_availibility(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
         redirect_to new_ticket_payment_url(@ticket.id)
       else
-       redirect_to new_ticket_passenger_path(@ticket.id)
+       redirect_to new_ticket_passenger_path(@ticket.id),notice:"Passenger Name Already Registered"
        end
     elsif params[:add_more].present?
       if @passenger.save
-        update_seat_availibility(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
+        #update_seat_availibility(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
         # Redirect to the same page to add more passengers
         redirect_to new_ticket_passenger_path(@ticket.id),notice:"Successfully Add passenger"
       else
@@ -59,84 +62,14 @@ class PassengersController < ApplicationController
       end
     else
       if @passenger.save
-        update_seat_availibility(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
+        #update_seat_availibility(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
         redirect_to new_ticket_passenger_path(@ticket.id)
       else
-       redirect_to new_ticket_passenger_path(@ticket.id)
+       redirect_to new_ticket_passenger_path(@ticket.id),notice:"Passenger Name Already Registered"
        end
     end
     
   end
-
-#set_seat_no
-def set_seat_no(train_id,class_type,seat_type)
-  train=Train.find(train_id)
-    
-  if(train.seats.where(class_type: class_type).where(seat_type:seat_type).first.seat_quantity!=0)
-    @passenger.seat_id=train.seats.where(class_type: class_type).where(seat_type:seat_type).first.id
-    @passenger.seat_no=train.seats.where(class_type: class_type).where(seat_type:seat_type).first.seat_quantity
-    @passenger.class_type = class_type
-    @passenger.seat_type = seat_type 
-    
-  elsif(train.seats.where(class_type:"AC").where(seat_type:"LB").first.seat_quantity!=0)
-    @passenger.seat_id=train.seats.where(class_type:"AC").where(seat_type:"LB").first.id
-    @passenger.seat_no=train.seats.where(class_type:"AC").where(seat_type:"LB").first.seat_quantity
-    @passenger.class_type = "AC"
-    @passenger.seat_type = "LB" 
-
-  elsif(train.seats.where(class_type:"AC").where(seat_type:"UB").first.seat_quantity!=0)
-    @passenger.seat_id=train.seats.where(class_type:"AC").where(seat_type:"UB").first.id
-    @passenger.seat_no=train.seats.where(class_type:"AC").where(seat_type:"UB").first.seat_quantity
-    @passenger.class_type = "AC"
-    @passenger.seat_type = "UB" 
-
-  elsif(train.seats.where(class_type:"AC").where(seat_type:"MB").first.seat_quantity!=0)
-    @passenger.seat_id=train.seats.where(class_type:"AC").where(seat_type:"MB").first.id
-
-    @passenger.seat_no=train.seats.where(class_type:"AC").where(seat_type:"MB").first.seat_quantity
-    @passenger.class_type = "AC"
-    @passenger.seat_type = "MB" 
-
-  elsif(train.seats.where(class_type:"SL").where(seat_type:"LB").first.seat_quantity!=0)
-    @passenger.seat_id=train.seats.where(class_type:"SL").where(seat_type:"LB").first.id
-
-    @passenger.seat_no=train.seats.where(class_type:"SL").where(seat_type:"LB").first.seat_quantity
-    @passenger.class_type = "SL"
-    @passenger.seat_type = "LB" 
-
-  elsif(train.seats.where(class_type:"SL").where(seat_type:"MB").first.seat_quantity!=0)
-
-    @passenger.seat_id=train.seats.where(class_type:"SL").where(seat_type:"MB").first.id
-
-    @passenger.seat_no=train.seats.where(class_type:"SL").where(seat_type:"MB").first.seat_quantity
-    @passenger.class_type = "SL"
-    @passenger.seat_type = "MB" 
-
-  elsif(train.seats.where(class_type:"SL").where(seat_type:"UB").first.seat_quantity!=0)
-
-    @passenger.seat_id=train.seats.where(class_type:"SL").where(seat_type:"UB").first.id
-
-    @passenger.seat_no=train.seats.where(class_type:"SL").where(seat_type:"UB").first.seat_quantity
-    @passenger.class_type = "SL"
-    @passenger.seat_type = "UB" 
-
-  else
-    @passenger.seat_no=0
-    @passenger.class_type = class_type
-    @passenger.seat_type = seat_type 
-  end
-end
-
-#update seat_quantity
-def update_seat_availibility(train_id,class_type,seat_type)
-  train=Train.find(train_id)
-  seat=train.seats.where(class_type:class_type).where(seat_type:seat_type)
-  quantity=Seat.find(seat.ids.join.to_i).seat_quantity
-   if(quantity==0)
-    @ticket.seat_quantity=0
-   end
-   seat.update(seat_quantity:quantity-1)
-end
 
   private
     def set_passenger
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -21,21 +21,19 @@ class PaymentsController < ApplicationController
       @ticket.fare = @payment.payment_amount
       @ticket.save
       #update seat quantity
-    #  @passengers = @ticket.passengers
-    #  @passengers.each do |passenger|
-    #     if passenger.seat_no == 0
-    #        update_seat_availibility(@ticket.train_id,passenger.class_type,passenger.seat_type)
-    #     end
-    #  end
+     @passengers = @ticket.passengers
+        # @passenger = @passengers.first
+        # set_seat_no(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
+        # @passenger.save
+        # update_seat_availibility(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
+     @passengers.each do | passenger|
+          @passenger = passenger
+          set_seat_no(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
+          @passenger.save
+          update_seat_availibility(@ticket.train_id,@passenger.class_type,@passenger.seat_type)
+     end
       redirect_to @ticket      
     else
-      @passengers.each do |passenger|
-         if passenger.seat_no!=0
-            passenger.class_type = "NA"
-            passenger.seat_type = "NA"
-            passenger.seat_id = "nil"
-         end
-      end
       redirect_to new_ticket_path(train_id: @ticket.train_id), notice:"Validation Failed"
      end
   end
@@ -44,8 +42,65 @@ class PaymentsController < ApplicationController
     @payment=Payment.find_by(ticket_id:params[:id])
   end
   
-
+#set_seat_no
+def set_seat_no(train_id,class_type,seat_type)
+  train=Train.find(train_id)
   
+  if(train.seats.where(class_type: class_type).where(seat_type:seat_type).first.seat_quantity!=0)
+    @passenger.seat_id=train.seats.where(class_type: class_type).where(seat_type:seat_type).first.id
+    @passenger.seat_no=train.seats.where(class_type: class_type).where(seat_type:seat_type).first.seat_quantity
+    @passenger.class_type = class_type
+    @passenger.seat_type = seat_type 
+    
+  elsif(train.seats.where(class_type:"AC").where(seat_type:"LB").first.seat_quantity!=0)
+    @passenger.seat_id=train.seats.where(class_type:"AC").where(seat_type:"LB").first.id
+    @passenger.seat_no=train.seats.where(class_type:"AC").where(seat_type:"LB").first.seat_quantity
+    @passenger.class_type = "AC"
+    @passenger.seat_type = "LB" 
+
+  elsif(train.seats.where(class_type:"AC").where(seat_type:"UB").first.seat_quantity!=0)
+    @passenger.seat_id=train.seats.where(class_type:"AC").where(seat_type:"UB").first.id
+    @passenger.seat_no=train.seats.where(class_type:"AC").where(seat_type:"UB").first.seat_quantity
+    @passenger.class_type = "AC"
+    @passenger.seat_type = "UB" 
+
+  elsif(train.seats.where(class_type:"AC").where(seat_type:"MB").first.seat_quantity!=0)
+    @passenger.seat_id=train.seats.where(class_type:"AC").where(seat_type:"MB").first.id
+
+    @passenger.seat_no=train.seats.where(class_type:"AC").where(seat_type:"MB").first.seat_quantity
+    @passenger.class_type = "AC"
+    @passenger.seat_type = "MB" 
+
+  elsif(train.seats.where(class_type:"SL").where(seat_type:"LB").first.seat_quantity!=0)
+    @passenger.seat_id=train.seats.where(class_type:"SL").where(seat_type:"LB").first.id
+
+    @passenger.seat_no=train.seats.where(class_type:"SL").where(seat_type:"LB").first.seat_quantity
+    @passenger.class_type = "SL"
+    @passenger.seat_type = "LB" 
+
+  elsif(train.seats.where(class_type:"SL").where(seat_type:"MB").first.seat_quantity!=0)
+
+    @passenger.seat_id=train.seats.where(class_type:"SL").where(seat_type:"MB").first.id
+
+    @passenger.seat_no=train.seats.where(class_type:"SL").where(seat_type:"MB").first.seat_quantity
+    @passenger.class_type = "SL"
+    @passenger.seat_type = "MB" 
+
+  elsif(train.seats.where(class_type:"SL").where(seat_type:"UB").first.seat_quantity!=0)
+
+    @passenger.seat_id=train.seats.where(class_type:"SL").where(seat_type:"UB").first.id
+
+    @passenger.seat_no=train.seats.where(class_type:"SL").where(seat_type:"UB").first.seat_quantity
+    @passenger.class_type = "SL"
+    @passenger.seat_type = "UB" 
+
+  else
+    @passenger.seat_no=0
+    @passenger.class_type = class_type
+    @passenger.seat_type = seat_type 
+  end
+end
+
 #update seat_quantity
 def update_seat_availibility(train_id,class_type,seat_type)
   train=Train.find(train_id)
@@ -54,10 +109,9 @@ def update_seat_availibility(train_id,class_type,seat_type)
    if(quantity==0)
     @ticket.seat_quantity=0
    end
-  seat.update(seat_quantity:quantity-1)
+   seat.update(seat_quantity:quantity-1)
 end
-
-
+  
   private
   def payment_params
     params.require(:payment).permit(:payment_method)
